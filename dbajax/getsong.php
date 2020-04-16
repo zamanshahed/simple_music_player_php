dd82a4c7a91886bee2decb1eb420580c6beb8493
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-16 10:41:54 +0600

    RATING SUCCESS: DB+AJAX part 5

--- dbajax/getsong.php
+++ dbajax/getsong.php
@@ -3,44 +3,50 @@
     if($mysqli->connect_error){
         exit('Failed to connect server');
     }
-    $sql = "SELECT `song_name`,`artist`,`rating`,`song_url` FROM `song_list` WHERE `song_name` = ?";
-    
+    $sql = "UPDATE `song_list` 
+    SET rating = ? 
+    WHERE song_list.song_id = 2";
+  
     $stmt  = $mysqli->prepare($sql);
-    $stmt->bind_param('s', $_GET['q']);
+    $stmt->bind_param('i', $_GET['q']);
     $stmt->execute();
-    $stmt->store_result();
-    $stmt->bind_result($song_name, $artist, $rating, $song_url);
-    $stmt->fetch();
+    echo '<script language="javascript">';
+    echo 'alert("UPDATED...!")';
+    echo '</script>';
+    // $stmt->store_result();
+    // $stmt->bind_result($song_name, $artist, $rating, $song_url);
+    // $stmt->fetch();
+    // $stmt->fetch();
     $stmt->close();
-
-    echo "
-        <table>
-            <tr>
-                <th>
-                    Song Name:
-                </th>
-                <td>
-                    " .$song_name. "
-                </td>
-                <th>
-                    Artist:
-                </th>
-                <td>
-                    ".$artist."
-                </td>
-                <th>
-                    Rating:
-                </th>
-                <td>
-                    ".$rating."
-                </td>
-                <th>
-                    URL:
-                </th>
-                <td>
-                    ".$song_url."
-                </td>
-            </tr>
-        </table>
-    ";
+    echo "Success..!";
+    // echo "
+    //     <table>
+    //         <tr>
+    //             <th>
+    //                 Song Name:
+    //             </th>
+    //             <td>
+    //                 " .$song_name. "
+    //             </td>
+    //             <th>
+    //                 Artist:
+    //             </th>
+    //             <td>
+    //                 ".$artist."
+    //             </td>
+    //             <th>
+    //                 Rating:
+    //             </th>
+    //             <td>
+    //                 ".$rating."
+    //             </td>
+    //             <th>
+    //                 URL:
+    //             </th>
+    //             <td>
+    //                 ".$song_url."
+    //             </td>
+    //         </tr>
+    //     </table>
+    // ";
 ?>
