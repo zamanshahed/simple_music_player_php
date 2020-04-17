23813fc022c283f155685e6ce4c0ffe92c0d520b
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-04-17 13:01:43 +0600

    PLAYLIST_1: sql prepared, have bug in php codes

--- addtolist.php
+++ addtolist.php
@@ -1,44 +1,55 @@
 <?php
-
+require_once('connection.php');
 $mysqli = new mysqli("localhost", "root", "", "wave");
 if($mysqli->connect_error){
     exit('Failed to connect server');
 }
 
-$sql = "
-  UPDATE
+$user_name = $_GET['u'];
+$user_id = 0;
+$list_id = $_GET['l'];
+$song_id = $_GET['s'];
+$sql1 = "SELECT id FROM `waver` WHERE name = '$user_name'";
+$result1 = mysqli_query($con,$sql1);
+
+while ($row = $result1->fetch_assoc()) {
+  $user_id =  $row['id'];
+}
+$sql2 = "
+UPDATE
     waving
   SET
-    user_id = 1,
-    list_id = 2,
-    list_name='FAV'
+    user_id = '$user_id',
+    list_id = '$list_id',
+    song_id = '$song_id'
   WHERE
-    user_id = 1
-    AND list_id = 2;
+    user_id = '$user_id'
+    AND list_id = '$list_id'
+    AND song_id='$song_id';
    
    INSERT INTO
     waving (
       user_id,
       list_id,
-      list_name
+      song_id
     )
   SELECT
-    1,
-    2,
-    'FAV'
+    '$user_id',
+    '$list_id',
+    '$song_id'
   WHERE
     NOT EXISTS (
-        SELECT * FROM waving WHERE user_id = 1 AND list_id = 2
+        SELECT * FROM waving WHERE user_id = '$user_id' AND list_id = '$list_id' AND song_id = '$song_id'
     )
 ";
 
-$stmt  = $mysqli->prepare($sql);
-$stmt->bind_param(
-  'sii', 
-  $_GET['u'], 
-  $_GET['l'],
-  $_GET['s']
-);
+$stmt  = $mysqli->prepare($sql2);
+// $stmt->bind_param(
+//   'sii', 
+//   $_GET['u'], 
+//   $_GET['l'],
+//   $_GET['s']
+// );
 $stmt->execute();
 $stmt->close();
 echo "Success..!";
